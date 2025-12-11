29707ad748537cc886e6b082d701c9cfcbab7c4a
sripatlu-sudo <sripatlu@gmail.com>
2025-12-11 09:45:11 -0600

    Update app-sidebar-c.py

--- app-sidebar-c.py
+++ app-sidebar-c.py
@@ -113,7 +113,7 @@ body, .stApp {
 .sidebar-title {
     font-size: 24px;
     font-weight: 700;
-    color: #FFD700;
+    color: #262730;
     font-family: "Playfair Display", serif;
     text-shadow: 1px 1px 2px rgba(0,0,0,0.5);
     margin-bottom: 20px;
