4cad7726012daefbbf8475469e32111728ec6556
sripatlu-sudo <sripatlu@gmail.com>
2025-12-14 10:26:36 -0600

    Update twilio.py

--- twilio.py
+++ twilio.py
@@ -1,8 +1,1 @@
-from twilio.rest import Client
-account_sid = ''
-auth_token = ''
-client = Client(account_sid, auth_token)
-message = client.messages.create(
-    to='[]'
-)
-print(message.sid)
+print("Hello")
