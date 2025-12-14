818dffd11b5c0aa68ce2e233d94f51254d24a20b
sripatlu-sudo <sripatlu@gmail.com>
2025-12-14 10:25:22 -0600

    files

--- twilio.py
+++ twilio.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
-account_sid = 'AC59097ed255cf8369cefef48e1ead1d92'
-auth_token = '299956a49d94fbb4c3c63a38e9dc49ac'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 message = client.messages.create(
-    to='[13145048971]'
+    to='[]'
 )
 print(message.sid)
